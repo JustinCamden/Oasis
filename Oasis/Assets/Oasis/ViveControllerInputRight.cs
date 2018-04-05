7f40bf40fba05e1c9648c91dc9f67f94b8399c29
Unknown <camden@usc.edu>
2018-04-05 11:03:37 -0700

    Added teleportation snf vr

---
+++ Oasis/Assets/Oasis/ViveControllerInputRight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInputRight : MonoBehaviour {
+
+    // Tracked object ref
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_TrackedController controller;
+    private SteamVR_Controller.Device device
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.PadClicked += ControllerPadClicked;
+    }
+
+   private void ControllerPadClicked(object sender, ClickedEventArgs e)
+    {
+        Debug.Log("Clicked");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
