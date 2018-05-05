9d4e9302e06ce5873f6d28d1f5ff676d7398bd0c
Unknown <camden@usc.edu>
2018-05-05 00:19:07 -0700

    Working on inheriting from VR expac

---
+++ Oasis/Assets/HeatMapper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatMapper : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject heatMap;
+    [SerializeField]
+    private Color heatMapColor = Color.red;
+    [SerializeField]
+    private Material heatmapMaterial;
+
+    List<GameObject> heatMaps;
+
+    [SerializeField]
+    private float spaceBetweenMaps = 2f;
+    Vector3 lastPosition;
+
+	// Use this for initialization
+	void Start () {
+        heatMaps = new List<GameObject>();
+        heatmapMaterial = GetComponent<MeshRenderer>().material;
+        heatmapMaterial.SetColor("_EmissionColor", heatMapColor);
+        lastPosition = transform.position;
+        Clock.worldClock.onDayReset += OnDayReset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 directionVector = transform.position - lastPosition;
+        float distance = directionVector.magnitude;
+        if (distance > spaceBetweenMaps)
+        {
+            directionVector = Vector3.Normalize(directionVector);
+            while (distance > spaceBetweenMaps)
+            {
+                Vector3 spawnPosition = lastPosition;
+                spawnPosition += directionVector * distance;
+                SpawnHeatMap(spawnPosition);
+                distance -= spaceBetweenMaps;
+            }
+            lastPosition = transform.position;
+        }
+		
+	}
+
+    void SpawnHeatMap(Vector3 spawnPosition)
+    {
+        GameObject newMap = Instantiate(heatMap, spawnPosition, Quaternion.identity);
+        heatMap.GetComponent<MeshRenderer>().material = heatmapMaterial;
+        heatMaps.Add(newMap);
+    }
+
+    void OnDayReset()
+    {
+        foreach (GameObject currMap in heatMaps)
+        {
+            Destroy(currMap, 0.0001f);
+        }
+        heatMaps.Clear();
+    }
+}
